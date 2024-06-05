a179c50a751177a6d7eaebbb7b6604a2b4149ecf
Luis Felipe Hau <luisfelipehau@gmail.com>
2024-06-05 16:09:35 -0300

    rota de delete finalizada

--- app.py
+++ app.py
@@ -84,8 +84,11 @@ def update_user(id_user):
 @login_required
 def delete_user(id_user):
   user = User.query.get(id_user)
-
+  if id_user == current_user.id:
+    return jsonify({"message": "Deleção não permitida"}), 403
   if user:
+    db.session.delete(user)
+    db.session.commit()
     return jsonify({"message": f"Usuário {id_user} deletado com sucesso"})
 
   return jsonify({"message": "Usuario não encontrado"}), 404
